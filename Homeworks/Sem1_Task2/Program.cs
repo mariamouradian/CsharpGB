8ddb664d0024e1e29c0691d74844464259f77c8b
Maria_Mouradian <mariamouradian@yandex.ru>
2024-09-24 00:28:06 +0300

    Task 2 solved

--- Homeworks/Sem1_Task2/Program.cs
+++ Homeworks/Sem1_Task2/Program.cs
@@ -4,5 +4,13 @@
 //4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
 //2 => -2, -1, 0, 1, 2
 
-Console.WriteLine("Введите целое положительное число: "):
+Console.WriteLine("Введите целое положительное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+
+int count = -num; //num * -1
+
+while (count <= num)
+    {
+    Console.Write(count + " ");
+    count = count + 1; //count++
+    }
