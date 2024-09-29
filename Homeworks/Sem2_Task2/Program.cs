b73b78109f5b84eb71498c66d705e21250f84116
Maria_Mouradian <mariamouradian@yandex.ru>
2024-09-29 15:23:32 +0300

    Sem2_Task2 added

---
+++ Homeworks/Sem2_Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и возводит вторую цифру этого числа в степень, равную третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+Console.Write("Введите целое трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a >= 100 && a <= 999)
+{
+    int secondDigit = a / 10 % 10; //256 / 100 = 2.56 = 2
+    int thirdDigit = a % 10; //256 % 10 = 250 + 6 = 6
+
+    int result = secondDigit;
+    int count = 1;
+
+    while (count < thirdDigit)
+    {
+        result = result * secondDigit; // result *= secondDigit
+        count++;
+    }
+    Console.Write(result);
+}
+else
+{
+    Console.Write("Число за пределами диапазона!");
+}
