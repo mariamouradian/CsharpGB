8d91c84ba7d5f77413d90e764774c76d5ca455f0
Maria_Mouradian <mariamouradian@yandex.ru>
2024-09-29 13:42:41 +0300

    HW3 resolved

---
+++ Homeworks/HW_Sem1_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 0)
+{
+    Console.WriteLine("Число положительное.");
+}
+else if (a < 0)
+{
+    Console.WriteLine("Число отрицательное.");
+}
+else
+{
+    Console.WriteLine("Число ноль.");
+}
