3d5ef5cf65189258d73dea235598d00652ba13de
Maria_Mouradian <mariamouradian@yandex.ru>
2024-09-29 13:19:27 +0300

    HW1 resolved

---
+++ Homeworks/HW_Sem1_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (secondNumber == 0)
+{
+    Console.Write("На ноль делить нельзя!");
+}
+else if (firstNumber % secondNumber == 0)
+{
+    Console.Write("Первое число делится на второе без остатка.");
+}
+else
+{
+    Console.Write("Первое число не делится на второе без остатка.");
+}
