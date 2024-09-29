7be4d42f020468ac0503418c059b159f2ac3c84d
Maria_Mouradian <mariamouradian@yandex.ru>
2024-09-29 18:44:12 +0300

    HW4 resolved

---
+++ Homeworks/HW_Sem2_4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую.
+
+Console.Write("Введите натуральное: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 10)
+{
+    Console.Write(a);
+}
+else
+{
+    while (a > 0)
+    {
+        int currentDigit = a % 10;
+        a /= 10;
+        if (a > 0)
+        { 
+            Console.Write(currentDigit + ","); 
+        }
+        else 
+        { 
+            Console.Write(currentDigit); 
+        }
+    }
+}
