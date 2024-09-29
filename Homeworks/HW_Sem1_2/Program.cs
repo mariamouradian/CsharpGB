4e53f09b779625311a0d3b4c0399256f39e19c7b
Maria_Mouradian <mariamouradian@yandex.ru>
2024-09-29 13:37:12 +0300

    HW2 resolved

---
+++ Homeworks/HW_Sem1_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if ((a >= b && a <= c ) || (a >= c && a <=b ))
+{
+    Console.WriteLine(a);
+}
+else if ((b >= a && b <= c) || (b >= c && b <= a))
+{
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine(c);
+}
