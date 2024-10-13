e058bb3090e1ec468f1beaa58626896c97701751
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-13 21:42:10 +0300

    Sem4_Task1 added

---
+++ Homeworks/Sem4_Task1/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Определите количество простых чисел в этом массиве.
+// Примеры
+// [1 3 4 19 3] => 2
+// [4 3 4 1 9 5 21 13]  => 3
+
+/*int[] array = new int[5];*/
+
+//int - тип возвращаемого значения, тип функции, тип метода
+//CreateArrayRndInt - идентификатор (название)
+//() - исходные данные, параметры
+//{} - тело функции, тело метода, исходный код
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max); // () - аргумент
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i <  array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int CountPrimeNembers(int[] array)
+{
+    int count = 0;
+// реализация
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (IsPrime(array[i]))
+        {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+bool IsPrime (int num)
+{
+    for (int i = 2; i <= Math.Sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int[] arr = CreateArrayRndInt(10, 1, 100);
+PrintArray(arr);
+
+int countPrime = CountPrimeNembers(arr);
+Console.Write($" => {countPrime}");
