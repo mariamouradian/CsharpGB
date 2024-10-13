23887b3cacb4501b75a7a52d32b34879f78a1fbd
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-13 15:28:18 +0300

    HW_Sem3_Task1 added

---
+++ Homeworks/HW_Sem3_Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задание 1:
+// Поиск элемента в массиве
+// Описание: Задайте массив целых чисел. Напишите программу, которая проверяет,
+// присутствует ли заданное число в массиве.
+// Программа должна вывести: Присутствует/Не присутствует.
+
+int[] array = new int[5] {25, 64, 11, 98, 154};
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool isExistNum = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == number)
+    {
+        isExistNum = true;
+        break;
+    }
+
+}
+// тернарный оператор (Если ..., то выводим Да, иначе выаодим Нет)
+string result = isExistNum ? "Присутствует" : "Не присутствует";
+
+// Console.WriteLine(result);
+Console.WriteLine(isExistNum ? "Присутствует" : "Не присутствует");
