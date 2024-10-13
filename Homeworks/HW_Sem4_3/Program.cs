6f87182c0397fd20d2aaf648a14d1d43ba7d12c6
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-13 23:59:58 +0300

    HW_Sem4_3

---
+++ Homeworks/HW_Sem4_3/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max); // () - аргумент
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int RevertArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp;
+        temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+    foreach (int temp in array)
+    {
+        Console.Write(temp + " ");
+    }
+}
+
+Console.WriteLine("Задайте размер массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArrayRndInt(a, 100, 1000);
+PrintArray(arr);
+
+int result = RevertArray(arr);
+Console.WriteLine("Перевернутый массив: ");
+
+Console.WriteLine(result);
