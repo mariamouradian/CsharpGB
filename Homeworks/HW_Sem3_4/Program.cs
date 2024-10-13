cb63fcf09ca8328f0863b0bdf81566b0d6d61d92
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-13 15:08:52 +0300

    HW_Sem3_4 added

---
+++ Homeworks/HW_Sem3_4/Program.cs
@@ -0,0 +1,26 @@
+﻿int num;
+Console.Write("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int numCopy = num;
+int count = 0;
+while (numCopy != 0)
+{
+    numCopy /= 10;
+    count++;
+}
+
+int[] arr = new int[count];
+int index = 0;
+while (num != 0)
+{
+    arr[index] = num % 10;
+    num /= 10; // num = mum / 10
+    index++;
+}
+Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"{arr[i]} ");
+}
+Console.Write("]");
