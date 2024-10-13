3cf7e578960dee88b2428b4eb54f2555d93650fa
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-13 16:00:47 +0300

    HW_Sem3_Task3 added

---
+++ Homeworks/HW_Sem3_Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задание 3:
+// Поиск среднего значения массива
+// Описание: Задайте массив из 10 целых чисел.
+// Найдите среднее значение элементов массива.
+
+using System.Diagnostics.CodeAnalysis;
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11 };
+double sum = 0;
+double result = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    sum = sum + array[i];
+}
+
+for (int i = 0;i < array.Length; i++)
+{
+    result = sum / array.Length;
+}
+
+Console.Write(result);
