5a6218619907b31c3b5aa28f2af2756b2cd7fde7
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-13 15:08:23 +0300

    HW_Sem3_3 added

---
+++ Homeworks/HW_Sem3_3/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 3, 2, 4, 2, 3 };
+int[] arrayResult = new int[array.Length / 2];
+
+int left = 0;
+int right = array.Length - 1;
+
+while (left < right)
+{
+    arrayResult[left] = array[left] * array[right];
+    left++;
+    right--;
+}
+for (int i = 0; i < arrayResult.Length; i++)
+{
+    Console.Write(arrayResult[i] + " ");
+}
