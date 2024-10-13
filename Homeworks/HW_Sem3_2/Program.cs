4c91c59a4ce01581b81244dc7e3a111cf13ec071
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-13 15:08:10 +0300

    HW_Sem3_2 added

---
+++ Homeworks/HW_Sem3_2/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = new int[10] {-3, 4, -5, 6, 8, -4, 7, 9, -1, 3};
+
+for (int i = 0; i < array.Length; i++)
+{
+    //array[i] = array[i] * -1;
+    array[i] *= -1;
+}
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
