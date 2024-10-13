767c3e3f8688ce68441efc0f93adf2dbead57914
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-14 01:36:05 +0300

    Task3 added

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3
+// Считать с консоли строку, состоящую из цифр и латинских букв.
+// Сформировать новую строку, состоящую из букв исходной строки.
+
+string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
