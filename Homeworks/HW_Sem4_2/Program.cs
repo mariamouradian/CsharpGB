0f5949df6eee4eded8f631d062f439af5672908e
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-13 23:15:10 +0300

    HW_Sem4_2

---
+++ Homeworks/HW_Sem4_2/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max); // () - аргумент
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int CountEvenNums(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+Console.WriteLine("Задайте размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArrayRndInt(n, 100, 1000);
+PrintArray(arr);
+
+int result = CountEvenNums(arr);
+Console.Write($" => {result}");
