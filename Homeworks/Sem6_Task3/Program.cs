ceccf1c8a6cdc2cf9aac08733b3c6b2307d42bfb
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-20 19:59:47 +0300

    Sem6_Task3 added

---
+++ Homeworks/Sem6_Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Считать строку с консоли, состоящую из латинских букв в нижнем регистре. 
+// Выяснить, сколько среди введённых букв гласных. 
+// Пример 
+// “hello” => 2 
+// “world” => 1
+
+int CountVowels(string str)
+{
+    string vowels = "aeiouy";
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if (vowels[j] == str[i])
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Вводите слово: ");
+string start = Console.ReadLine();
+Console.WriteLine($"В слове {CountVowels(start)} гласных.");
