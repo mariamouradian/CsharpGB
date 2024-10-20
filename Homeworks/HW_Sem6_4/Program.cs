5bd4c9b6e05d1c7ad8985b78dd216803cbf288a8
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-20 21:45:58 +0300

    HW_Sem6_4 added

---
+++ Homeworks/HW_Sem6_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4*(не обязательная): 
+// Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+string ReverseWords(string str)
+{
+    string[] words = str.Split(' ');
+    Array.Reverse(words);
+    return string.Join(" ",words);
+}
+
+Console.WriteLine("Вводите слова, разделенные пробелами: ");
+string input = Console.ReadLine();
+string result = ReverseWords(input);
+Console.WriteLine(result);
