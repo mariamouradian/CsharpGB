ab2beddf2976fa53f40bc954340e5d25091b6140
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-20 21:04:39 +0300

    HW_Sem6_3 added

---
+++ Homeworks/HW_Sem6_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3: 
+// Задайте произвольную строку. 
+// Выясните, является ли она палиндромом.
+
+bool IsPolindrome(string str)
+{
+    string normalized = new string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+    return normalized.SequenceEqual(normalized.Reverse());
+}
+
+
+// string input = "шалаш";
+Console.WriteLine("Вводите слово: ");
+string input = Console.ReadLine();
+bool isPolindrome = IsPolindrome(input);
+Console.WriteLine(isPolindrome?"Yes":"No");
