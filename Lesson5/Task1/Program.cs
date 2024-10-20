fa429d05a68519576e2e85a74044561e21bfa940
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-20 15:14:13 +0300

    Task1 added

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 1 
+• Вычислить факториал от натурального числа N*/
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+Console.WriteLine(Fact(5));
