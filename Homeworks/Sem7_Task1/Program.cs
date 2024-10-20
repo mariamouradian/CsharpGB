2c768ad5f32c15f159ee9d4be558134af93bc0d7
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-20 22:26:42 +0300

    Sem7_Task1 resolved

---
+++ Homeworks/Sem7_Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задание 1. 
+// Рекурсия 
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// Указание Использовать рекурсию. 
+// Пример 123 => 6 63 => 9
+
+void PrintNaturalNumbers(int num)
+{
+    if(num == 0) return;
+    Console.Write($"{num} "); // вывод в обратном порядке
+    PrintNaturalNumbers(num - 1);
+    // Console.Write($"{num} "); - вывод в прямом порядке
+}
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintNaturalNumbers(number);
+
