8b7b62009b5e202c55b2461ae9921475516057d3
Maria_Mouradian <mariamouradian@yandex.ru>
2024-10-20 20:31:21 +0300

    HW_Sem6_1 added

---
+++ Homeworks/HW_Sem6_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: 
+// Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+
+string CreateStringFrom2DArray(char[,] array)
+{
+    string res = "";
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            res += array[i, j];
+        }
+    }
+    return res;
+}
+
+char[,] charArray = new char[,] { { 'a', 'b' }, { 'c', 'd' }, { 'e', 'f' } };
+string result = CreateStringFrom2DArray(charArray);
+Console.WriteLine(result);
